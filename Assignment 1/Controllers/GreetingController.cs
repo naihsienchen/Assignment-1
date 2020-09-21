51b81fa7e6c55098fc96ee80ff7eb312a51396ca
naihsienchen <nhc0629@gmail.com>
2020-09-21 18:05:25 -0400

    Add project files.

---
+++ Assignment 1/Controllers/GreetingController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Assignment_1.Controllers
+{
+    public class GreetingController : ApiController
+    {
+
+        // POST api/Greeting
+        public IEnumerable<string> Post ()
+        {
+            return new string[] { "Hello World!" };
+        }
+
+        // GET api/Greeting/{id}
+        public IEnumerable<string> Get(int id)
+        {
+            return new string[] { "Hello to " + id + " people!" };
+        }
+
+    }
+}
---
+++ Assignment 1/Controllers/SquareController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Assignment_1.Controllers
+{
+    public class SquareController : ApiController
+    {
+        /// <summary>
+        /// This method returns id square integer when receiving a get request with an id.
+        /// <example>GET api/square/{id}</example>
+        /// </summary>
+        /// <returns>[ id * id ]</returns>
+        public IEnumerable<int> Get(int id)
+        {
+            return new int[] { id * id };
+        }
+
+
+
+    }
+}
